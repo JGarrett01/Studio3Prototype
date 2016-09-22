cf4780712792eae1e45ade86bca5a984c74b7b69
SAE\jmgarrett <jake-garrett@hotmail.com>
2016-09-22 19:26:17 +1000

    + swooping birds scene + added square for player + added circles for birds +circles spawn at random spawn point and move towards player + circles can be clicked to be destroyed

---
+++ Assets/CircleController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleController : MonoBehaviour
+{
+
+    public GameObject player;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+
+    void OnMouseDown()
+    {
+        Destroy(this.gameObject);
+    }       
+            
+               
+            
+          
+}
+  
